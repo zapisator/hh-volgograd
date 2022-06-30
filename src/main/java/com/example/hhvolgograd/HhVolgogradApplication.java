882c04f175c9910662e6333cfd954685eb6306ae
zapisator <zapisator@logan-club.ru>
2022-06-30 13:01:11 +0300

    Initial

---
+++ src/main/java/com/example/hhvolgograd/HhVolgogradApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhvolgograd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhVolgogradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhVolgogradApplication.class, args);
+	}
+
+}
