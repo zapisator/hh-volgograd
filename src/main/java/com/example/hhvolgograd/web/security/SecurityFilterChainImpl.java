b4f2a85b268a97709354a320dcc83bd4e42fe70b
zapisator <zapisator@logan-club.ru>
2022-07-22 01:15:01 +0300

    Security chain with jwt bearer token auth without redundant filters

---
+++ src/main/java/com/example/hhvolgograd/web/security/SecurityFilterChainImpl.java
@@ -0,0 +1,37 @@
+package com.example.hhvolgograd.web.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityFilterChainImpl {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .servletApi(AbstractHttpConfigurer::disable)
+                .securityContext(AbstractHttpConfigurer::disable)
+                .requestCache(AbstractHttpConfigurer::disable)
+                .exceptionHandling(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(auth2ResourceServerConfigurer -> auth2ResourceServerConfigurer
+                        .jwt().and()
+                )
+                .authorizeRequests(
+                        authorizeRequestsConfigurer -> authorizeRequestsConfigurer
+                                .antMatchers(HttpMethod.GET, "/auth/login**").permitAll()
+                                .anyRequest().authenticated()
+                                .and()
+                )
+                .build();
+    }
+}
