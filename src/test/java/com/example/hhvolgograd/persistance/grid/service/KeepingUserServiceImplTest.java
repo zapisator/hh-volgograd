081766bbf1b6f21cb5dbdf27eb0c9685030f5104
zapisator <zapisator@logan-club.ru>
2022-07-30 00:34:10 +0300

    I moved out a common setup of these tests. Now they even are faster. Also, I removed some teardown logic.

--- src/test/java/com/example/hhvolgograd/persistance/grid/service/KeepingUserServiceImplTest.java
+++ src/test/java/com/example/hhvolgograd/persistance/grid/service/KeepingUserServiceImplTest.java
@@ -1,9 +1,6 @@
 package com.example.hhvolgograd.persistance.grid.service;
 
 import com.example.hhvolgograd.exception.TooEarlyToContactServiceException;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.core.DistributedObject;
 import lombok.val;
 import org.junit.jupiter.api.AfterEach;
@@ -14,27 +11,16 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.core.io.InputStreamResource;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.time.Duration;
-import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static java.io.File.separator;
 import static java.lang.Thread.sleep;
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
@@ -42,30 +28,20 @@ import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUti
 @Testcontainers
 class KeepingUserServiceImplTest {
 
-    private HazelcastManager factory;
+    @Container
+    private static final DockerComposeContainer<?> hazelcastMemberContainer;
+    private static final HazelcastManager factory;
+
     private KeepingUserService service;
 
-    @Container
-    public static DockerComposeContainer<?> hazelcastMemberContainer;
-
     static {
-        val containerExposedPort = 5701;
-        val path = System.getProperty("user.dir") + separator + "containers" + separator + "docker-compose.yaml";
-        val file = new File(path);
-        val serviceName = "hazelcast-member";
-
-        hazelcastMemberContainer = new DockerComposeContainer<>(file)
-                .withExposedService(serviceName, containerExposedPort, Wait.forListeningPort());
+        hazelcastMemberContainer = new HazelcastMemberContainerFactory().hazelcastMemberContainer();
         hazelcastMemberContainer.start();
+        factory = new HazelcastManagerTestFactory().factory();
     }
 
     @BeforeEach
     public void setUp() {
-        val properties = properties();
-        val clientConfig = clientConfig(properties);
-        val instance = HazelcastClient.newHazelcastClient(clientConfig);
-
-        factory = new HazelcastManager(instance);
         service = new KeepingUserServiceImpl(factory);
     }
 
@@ -75,10 +51,6 @@ class KeepingUserServiceImplTest {
                 .getInstance()
                 .getDistributedObjects()
                 .forEach(DistributedObject::destroy);
-        factory
-                .getInstance()
-                .shutdown();
-        factory = null;
     }
 
     @ParameterizedTest
@@ -119,14 +91,14 @@ class KeepingUserServiceImplTest {
 
     @Test
     void save_afterHaveAlreadyBeenSavedAndNotYetExpired_throws()
-            throws NoSuchFieldException, IllegalAccessException, InterruptedException
-    {
+            throws NoSuchFieldException, IllegalAccessException, InterruptedException {
         val email = randomAlphanumeric(20);
         val userJson = randomAlphanumeric(20);
         val secondsLessThanStorageTime = new Random().nextInt(KeepingUserServiceImpl.storageTime);
         val sleepPeriod = Duration.ofSeconds(secondsLessThanStorageTime).toMillis();
 
-        onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.SECONDS);
+        new HazelcastClientUnitChanger()
+                .onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.SECONDS, KeepingUserServiceImpl.class);
         service.save(email, userJson);
         sleep(sleepPeriod);
 
@@ -138,7 +110,8 @@ class KeepingUserServiceImplTest {
         val email = randomAlphanumeric(20);
         val userJson = randomAlphanumeric(20);
 
-        onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.NANOSECONDS);
+        new HazelcastClientUnitChanger()
+                .onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.NANOSECONDS, KeepingUserServiceImpl.class);
         service.save(email, userJson);
         sleep(1);
 
@@ -171,49 +144,5 @@ class KeepingUserServiceImplTest {
         val email = RandomStringUtils.random(new Random().nextInt(100));
 
         assertDoesNotThrow(() -> service.findUserByEmail(email));
-    }
-
-    private static void onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit timeUnit)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = KeepingUserServiceImpl.class.getDeclaredField("unit");
-        field.setAccessible(true);
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        field.set(null, timeUnit);
-    }
-
-    private ClientConfig clientConfig(Properties properties) {
-        val clientConfig = new ClientConfig();
-        val clusterName = properties.getProperty("hazelcast-client.cluster-name");
-        val instanceName = properties.getProperty("hazelcast-client.instance-name");
-        val clientNetworkingConfig = clientNetworkingConfig(properties);
-
-        clientConfig.setClusterName(clusterName);
-        clientConfig.setInstanceName(instanceName);
-        clientConfig.setNetworkConfig(clientNetworkingConfig);
-        return clientConfig;
-    }
-
-    private ClientNetworkConfig clientNetworkingConfig(Properties properties) {
-        val clientNetworkingConfig = new ClientNetworkConfig();
-        val clusterMember = properties.getProperty("hazelcast-client.network.cluster-members[0]");
-        val redoOperation = (boolean) properties.get("hazelcast-client.network.redo-operation");
-
-        clientNetworkingConfig.addAddress(clusterMember);
-        clientNetworkingConfig.setRedoOperation(redoOperation);
-        return clientNetworkingConfig;
-    }
-
-    private Properties properties() {
-        try (InputStream input = this.getClass().getClassLoader().getResourceAsStream("hazelcast-client.yaml")) {
-            val yamlProcessor = new YamlPropertiesFactoryBean();
-
-            yamlProcessor.setResources(new InputStreamResource(requireNonNull(input)));
-            return yamlProcessor.getObject();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 }
--- src/test/java/com/example/hhvolgograd/persistance/grid/service/OtpServiceImplTest.java
+++ src/test/java/com/example/hhvolgograd/persistance/grid/service/OtpServiceImplTest.java
@@ -1,9 +1,6 @@
 package com.example.hhvolgograd.persistance.grid.service;
 
 import com.example.hhvolgograd.exception.TooEarlyToContactServiceException;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.core.DistributedObject;
 import lombok.val;
 import org.junit.jupiter.api.AfterEach;
@@ -14,56 +11,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.core.io.InputStreamResource;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.time.Duration;
-import java.util.Properties;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static java.io.File.separator;
 import static java.lang.Thread.sleep;
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 
 class OtpServiceImplTest {
 
-    private HazelcastManager factory;
+    @Container
+    private static final DockerComposeContainer<?> hazelcastMemberContainer;
+    private static final HazelcastManager factory;
+
     private OtpService service;
 
-    @Container
-    public static DockerComposeContainer<?> hazelcastMemberContainer;
-
     static {
-        val containerExposedPort = 5701;
-        val path = System.getProperty("user.dir") + separator + "containers" + separator + "docker-compose.yaml";
-        val file = new File(path);
-        val serviceName = "hazelcast-member";
-
-        hazelcastMemberContainer = new DockerComposeContainer<>(file)
-                .withExposedService(serviceName, containerExposedPort, Wait.forListeningPort());
+        hazelcastMemberContainer = new HazelcastMemberContainerFactory().hazelcastMemberContainer();
         hazelcastMemberContainer.start();
+        factory = new HazelcastManagerTestFactory().factory();
     }
 
     @BeforeEach
     public void setUp() {
-        val properties = properties();
-        val clientConfig = clientConfig(properties);
-        val instance = HazelcastClient.newHazelcastClient(clientConfig);
-
-        factory = new HazelcastManager(instance);
         service = new OtpServiceImpl(factory);
     }
 
@@ -73,10 +49,6 @@ class OtpServiceImplTest {
                 .getInstance()
                 .getDistributedObjects()
                 .forEach(DistributedObject::destroy);
-        factory
-                .getInstance()
-                .shutdown();
-        factory = null;
     }
 
     @ParameterizedTest
@@ -104,7 +76,8 @@ class OtpServiceImplTest {
         val secondsLessThanStorageTime = new Random().nextInt(KeepingUserServiceImpl.storageTime);
         val sleepPeriod = Duration.ofSeconds(secondsLessThanStorageTime).toMillis();
 
-        onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.SECONDS);
+        new HazelcastClientUnitChanger()
+                .onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.SECONDS, OtpServiceImpl.class);
         service.save(email);
         sleep(sleepPeriod);
 
@@ -115,7 +88,8 @@ class OtpServiceImplTest {
     void save_afterExpired_ok() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
         val email = randomAlphanumeric(20);
 
-        onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.NANOSECONDS);
+        new HazelcastClientUnitChanger()
+                .onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit.NANOSECONDS, OtpServiceImpl.class);
         service.save(email);
         sleep(1);
 
@@ -147,49 +121,5 @@ class OtpServiceImplTest {
         val email = RandomStringUtils.random(new Random().nextInt(100));
 
         assertDoesNotThrow(() -> service.findOtpByEmail(email));
-    }
-
-    private static void onceToSpeedUpTest_ChangeWithReflectionFinalTimeUnit(TimeUnit timeUnit)
-            throws NoSuchFieldException, IllegalAccessException {
-        Field field = OtpServiceImpl.class.getDeclaredField("unit");
-        field.setAccessible(true);
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-        field.set(null, timeUnit);
-    }
-
-    private ClientConfig clientConfig(Properties properties) {
-        val clientConfig = new ClientConfig();
-        val clusterName = properties.getProperty("hazelcast-client.cluster-name");
-        val instanceName = properties.getProperty("hazelcast-client.instance-name");
-        val clientNetworkingConfig = clientNetworkingConfig(properties);
-
-        clientConfig.setClusterName(clusterName);
-        clientConfig.setInstanceName(instanceName);
-        clientConfig.setNetworkConfig(clientNetworkingConfig);
-        return clientConfig;
-    }
-
-    private ClientNetworkConfig clientNetworkingConfig(Properties properties) {
-        val clientNetworkingConfig = new ClientNetworkConfig();
-        val clusterMember = properties.getProperty("hazelcast-client.network.cluster-members[0]");
-        val redoOperation = (boolean) properties.get("hazelcast-client.network.redo-operation");
-
-        clientNetworkingConfig.addAddress(clusterMember);
-        clientNetworkingConfig.setRedoOperation(redoOperation);
-        return clientNetworkingConfig;
-    }
-
-    private Properties properties() {
-        try (InputStream input = this.getClass().getClassLoader().getResourceAsStream("hazelcast-client.yaml")) {
-            val yamlProcessor = new YamlPropertiesFactoryBean();
-
-            yamlProcessor.setResources(new InputStreamResource(requireNonNull(input)));
-            return yamlProcessor.getObject();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
     }
 }
