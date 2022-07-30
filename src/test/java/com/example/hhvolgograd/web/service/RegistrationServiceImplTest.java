c957e247e0fc181062d47876d449dc329a086bef
zapisator <zapisator@logan-club.ru>
2022-07-30 22:01:54 +0300

    Refactoring

--- src/test/java/com/example/hhvolgograd/web/service/RegistrationServiceImplTest.java
+++ src/test/java/com/example/hhvolgograd/web/service/RegistrationServiceImplTest.java
@@ -3,11 +3,15 @@ package com.example.hhvolgograd.web.service;
 import com.example.hhvolgograd.mail.service.MailService;
 import com.example.hhvolgograd.persistance.db.model.User;
 import com.example.hhvolgograd.persistance.db.service.CashService;
+import com.example.hhvolgograd.persistance.grid.service.KeepingUserService;
 import com.example.hhvolgograd.persistance.grid.service.KeepingUserServiceImpl;
+import com.example.hhvolgograd.persistance.grid.service.OtpService;
 import com.example.hhvolgograd.persistance.grid.service.OtpServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.val;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.DuplicateKeyException;
 
@@ -22,23 +26,48 @@ import static org.mockito.Mockito.when;
 
 class RegistrationServiceImplTest {
 
-    private final Random random = new Random();
+    private CashService cashService;
+    private OtpService otpService;
+    private KeepingUserService keepingUserService;
+    private MailService mailService;
+
+    @BeforeEach
+    void setUp() {
+        keepingUserService = mock(KeepingUserServiceImpl.class);
+        otpService = mock(OtpServiceImpl.class);
+        mailService = mock(MailService.class);
+        cashService = mock(CashService.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        keepingUserService = null;
+        otpService = null;
+        mailService = null;
+        cashService = null;
+    }
 
     @Test
     void register_alreadyRegisteredEmail_throws() throws JsonProcessingException {
         val user = mock(User.class);
-        val keepingUserService = mock(KeepingUserServiceImpl.class);
-        val otpService = mock(OtpServiceImpl.class);
-        val mailService = mock(MailService.class);
-        val cashService = mock(CashService.class);
+        val userEmail = randomStringWithNonZeroLength();
+        val userJson = randomStringWithNonZeroLength();
         val registrationService = new RegistrationServiceImpl(cashService, otpService, keepingUserService, mailService);
 
-        when(user.getEmail()).thenReturn(RandomStringUtils.random(1 + random.nextInt(19)));
-        when(user.toJson()).thenReturn(RandomStringUtils.random(30 + random.nextInt(30)));
-        doNothing().when(keepingUserService).save(anyString(), anyString());
-        when(otpService.save(anyString())).thenReturn(RandomStringUtils.random(32));
-        doNothing().when(mailService).send(anyString(), anyString());
-        when(cashService.existsUserByEmail(anyString())).thenReturn(true);
+        when(user.getEmail())
+                .thenReturn(userEmail);
+        when(user.toJson())
+                .thenReturn(userJson);
+        doNothing()
+                .when(keepingUserService)
+                .save(anyString(), anyString());
+        when(otpService.save(anyString()))
+                .thenReturn(RandomStringUtils.random(32));
+        doNothing()
+                .when(mailService)
+                .send(anyString(), anyString());
+        when(cashService.existsUserByEmail(anyString()))
+                .thenReturn(true);
 
         assertThrows(DuplicateKeyException.class, () -> registrationService.register(user));
     }
@@ -46,18 +75,24 @@ class RegistrationServiceImplTest {
     @Test
     void register_notRegisteredYetEmail_ok() throws JsonProcessingException {
         val user = mock(User.class);
-        val keepingUserService = mock(KeepingUserServiceImpl.class);
-        val otpService = mock(OtpServiceImpl.class);
-        val mailService = mock(MailService.class);
-        val cashService = mock(CashService.class);
+        val userEmail = randomStringWithNonZeroLength();
+        val userJson = randomStringWithNonZeroLength();
         val registrationService = new RegistrationServiceImpl(cashService, otpService, keepingUserService, mailService);
 
-        when(user.getEmail()).thenReturn(RandomStringUtils.random(1 + random.nextInt(19)));
-        when(user.toJson()).thenReturn(RandomStringUtils.random(30 + random.nextInt(30)));
-        doNothing().when(keepingUserService).save(anyString(), anyString());
-        when(otpService.save(anyString())).thenReturn(RandomStringUtils.random(32));
-        doNothing().when(mailService).send(anyString(), anyString());
-        when(cashService.existsUserByEmail(anyString())).thenReturn(false);
+        when(user.getEmail())
+                .thenReturn(userEmail);
+        when(user.toJson())
+                .thenReturn(userJson);
+        doNothing()
+                .when(keepingUserService)
+                .save(anyString(), anyString());
+        when(otpService.save(anyString()))
+                .thenReturn(RandomStringUtils.random(32));
+        doNothing()
+                .when(mailService)
+                .send(anyString(), anyString());
+        when(cashService.existsUserByEmail(anyString()))
+                .thenReturn(false);
 
         assertDoesNotThrow(() -> registrationService.register(user));
     }
