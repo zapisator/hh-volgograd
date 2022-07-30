e4c3fe72240c5ca9b8ee15d1e79460e84440a754
zapisator <zapisator@logan-club.ru>
2022-07-31 01:28:51 +0300

    One more example changing of logic of the class: service has service logic, not repository one

--- src/main/java/com/example/hhvolgograd/persistance/db/service/DbCashService.java
+++ src/main/java/com/example/hhvolgograd/persistance/db/service/DbCashService.java
@@ -3,7 +3,10 @@ package com.example.hhvolgograd.persistance.db.service;
 import com.example.hhvolgograd.persistance.db.model.User;
 import com.example.hhvolgograd.persistance.db.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
 
 @Service
 @AllArgsConstructor
@@ -12,8 +15,10 @@ public class DbCashService implements CashService {
     private final UserRepository repository;
 
     @Override
-    public boolean existsUserByEmail(String email) {
-        return repository.existsUserByEmail(email);
+    public void checkIfNoSuchEmailIsRegistered(String email) {
+        if (repository.existsUserByEmail(email)) {
+            throw new DuplicateKeyException(format("User with '%s' email has already registered.", email));
+        }
     }
 
     @Override
