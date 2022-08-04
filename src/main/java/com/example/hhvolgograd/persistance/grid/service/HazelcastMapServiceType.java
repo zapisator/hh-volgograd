8d9d1ccde51bc732ac9d5943783d8317fd49d8dd
zapisator <zapisator@logan-club.ru>
2022-08-04 23:39:48 +0300

    Types of hazelcast services: otp and user keeper

---
+++ src/main/java/com/example/hhvolgograd/persistance/grid/service/HazelcastMapServiceType.java
@@ -0,0 +1,14 @@
+package com.example.hhvolgograd.persistance.grid.service;
+
+import lombok.Getter;
+
+@Getter
+public enum HazelcastMapServiceType {
+    OTP_SERVICE ("otp"),
+    KEEPING_USER_SERVICE ("kept-user");
+
+    private final String mapName;
+    HazelcastMapServiceType(String mapName) {
+        this.mapName = mapName;
+    }
+}
