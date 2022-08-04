423f4711491ae065a4fe2cb20477a8d569f97020
zapisator <zapisator@logan-club.ru>
2022-08-04 23:47:38 +0300

    new view to cache service made me a little to change a registration service

--- src/main/java/com/example/hhvolgograd/web/service/RegistrationServiceImpl.java
+++ src/main/java/com/example/hhvolgograd/web/service/RegistrationServiceImpl.java
@@ -3,37 +3,47 @@ package com.example.hhvolgograd.web.service;
 import com.example.hhvolgograd.mail.service.MailService;
 import com.example.hhvolgograd.persistance.db.model.User;
 import com.example.hhvolgograd.persistance.db.service.CashService;
-import com.example.hhvolgograd.persistance.grid.service.KeepingUserService;
-import com.example.hhvolgograd.persistance.grid.service.OtpService;
-import lombok.AllArgsConstructor;
+import com.example.hhvolgograd.persistance.grid.service.HazelcastFactory;
+import com.example.hhvolgograd.persistance.grid.service.HazelcastMapService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+import static com.example.hhvolgograd.persistance.grid.service.HazelcastMapServiceType.KEEPING_USER_SERVICE;
+import static com.example.hhvolgograd.persistance.grid.service.HazelcastMapServiceType.OTP_SERVICE;
 import static java.util.Objects.requireNonNull;
 
 @Service
-@AllArgsConstructor
 @Slf4j
 public class RegistrationServiceImpl implements RegistrationService {
 
     private final CashService cashService;
-    private final OtpService otpService;
-    private final KeepingUserService keepingUserService;
+    private final HazelcastMapService otpService;
+    private final HazelcastMapService keepingUserService;
     private final MailService mailService;
+
+    public RegistrationServiceImpl(CashService cashService, HazelcastFactory factory, MailService mailService) {
+        this.cashService = cashService;
+        this.otpService = factory.createInstance(OTP_SERVICE);
+        this.keepingUserService = factory.createInstance(KEEPING_USER_SERVICE);
+        this.mailService = mailService;
+    }
 
     @SneakyThrows
     public void register(User user) {
         val email = user.getEmail();
         val userJson = user.toJson();
+        val otp = UUID.randomUUID().toString();
 
         cashService.checkIfNoSuchEmailIsRegistered(email);
         log.debug("User to send:\n{}", userJson);
         keepingUserService.save(email, userJson);
 
-        val otp = otpService.save(email);
+        otpService.save(email, otp);
         log.debug("otp '{}'", otp);
         mailService.send(email, otp);
     }
@@ -42,13 +52,13 @@ public class RegistrationServiceImpl implements RegistrationService {
     public void confirmRegistration(String email, String otp) {
         requireNonNull(email, "Email could not be null");
         requireNonNull(otp, "Otp could not be null");
-        checkIfOtpIsCorrect(email, otp);
-        val user = keepingUserService.getUserOrThrow(email);
+        requireOtpIsCorrect(email, otp);
+        val user = User.fromJson(keepingUserService.read(email));
         cashService.save(user);
     }
 
-    private void checkIfOtpIsCorrect(String email, String otp) {
-        if (!otpService.getOtpOrThrow(email).equals(otp)) {
+    private void requireOtpIsCorrect(String email, String otp) {
+        if (!otpService.read(email).equals(otp)) {
             throw new UsernameNotFoundException("User email or password are incorrect.");
         }
     }
