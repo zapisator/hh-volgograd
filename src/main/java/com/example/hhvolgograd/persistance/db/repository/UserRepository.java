ea06b07f9d8c74539ad388f88d84e657364ac064
zapisator <zapisator@logan-club.ru>
2022-08-04 23:45:41 +0300

    I need the UserRepository to find user by Email

--- src/main/java/com/example/hhvolgograd/persistance/db/repository/UserRepository.java
+++ src/main/java/com/example/hhvolgograd/persistance/db/repository/UserRepository.java
@@ -4,9 +4,13 @@ import com.example.hhvolgograd.persistance.db.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsUserByEmail(String email);
+
+    Optional<User> findUserByEmail(String email);
 
 }
