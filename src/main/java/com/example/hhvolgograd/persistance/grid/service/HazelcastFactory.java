cfc02f21c0e786b66276f4e89111a6a7d62080c2
zapisator <zapisator@logan-club.ru>
2022-08-04 23:38:18 +0300

    Hazelcast factory now is a real factory which depends on types

---
+++ src/main/java/com/example/hhvolgograd/persistance/grid/service/HazelcastFactory.java
@@ -0,0 +1,37 @@
+package com.example.hhvolgograd.persistance.grid.service;
+
+import com.example.hhvolgograd.configuration.ProjectHazelcastProperty;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@Getter
+@AllArgsConstructor
+public class HazelcastFactory {
+
+    private final HazelcastInstance instance;
+    private final ProjectHazelcastProperty hazelcastProperty;
+
+    public HazelcastMapServiceImpl createInstance(HazelcastMapServiceType type) {
+        final IMap<String, String> map = instance.getMap(type.getMapName());
+        final Duration duration;
+
+        switch (type) {
+            case OTP_SERVICE:
+                duration = hazelcastProperty.getOtpStorageDuration();
+                break;
+            case KEEPING_USER_SERVICE:
+                duration = hazelcastProperty.getKeepUserStorageDuration();
+                break;
+            default:
+                throw new IllegalArgumentException("Type of Hazelcast service can not be null");
+        }
+        return new HazelcastMapServiceImpl(map, duration);
+    }
+
+}
