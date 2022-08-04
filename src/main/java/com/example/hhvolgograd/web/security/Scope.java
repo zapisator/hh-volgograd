0f8a94ead3f5ca4360ba957b2e673783fd6ccc5a
zapisator <zapisator@logan-club.ru>
2022-08-04 23:42:21 +0300

    Scopes are changed from interface constants to enum

--- src/main/java/com/example/hhvolgograd/web/security/Scope.java
+++ src/main/java/com/example/hhvolgograd/web/security/Scope.java
@@ -1,10 +1,19 @@
 package com.example.hhvolgograd.web.security;
 
-public interface Scope {
+import lombok.Getter;
 
-    String USERINFO_READ = "userinfo.read";
-    String USERINFO_WRITE = "userinfo.write";
-    String PROFILE_READ = "profile.read";
-    String PHONES_READ = "phones.read";
-    String PHONES_WRITE = "phones.write";
+@Getter
+public enum Scope {
+
+    USERINFO_READ ("userinfo.read"),
+    USERINFO_WRITE ("userinfo.write"),
+    PROFILE_READ ("profile.read"),
+    PHONES_READ ("phones.read"),
+    PHONES_WRITE ("phones.write");
+
+    final String value;
+
+    Scope(String value) {
+        this.value = value;
+    }
 }
