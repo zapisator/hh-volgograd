5917103d7268330585baafa8fddd6c2fef084087
zapisator <zapisator@logan-club.ru>
2022-08-06 23:46:09 +0300

    Now repository can not only page and sort, but also it can filter

--- src/main/java/com/example/hhvolgograd/persistance/db/repository/UserRepository.java
+++ src/main/java/com/example/hhvolgograd/persistance/db/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.example.hhvolgograd.persistance.db.repository;
 
 import com.example.hhvolgograd.persistance.db.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepositoryImplementation<User, Long> {
 
     boolean existsUserByEmail(String email);
 
