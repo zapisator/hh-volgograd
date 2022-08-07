fbd430b27af85899677d607b073f6e57ab89a83a
zapisator <zapisator@logan-club.ru>
2022-08-07 20:19:37 +0300

    View of parameters in swagger now: Specification: not required (@Nullable), auto converted (@Filter), not Json (schema type "string") Pageable: not json but parameters (@ParameterObject), with different default values (@PageableDefault)

--- src/main/java/com/example/hhvolgograd/web/rest/ResourceController.java
+++ src/main/java/com/example/hhvolgograd/web/rest/ResourceController.java
@@ -3,24 +3,51 @@ package com.example.hhvolgograd.web.rest;
 import com.example.hhvolgograd.persistance.db.model.User;
 import com.example.hhvolgograd.web.service.ResourceService;
 import com.turkraft.springfilter.boot.Filter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/resource")
 @AllArgsConstructor
+@Tag(
+        name = "User resources",
+        description = "List users by criterion, change ones data"
+)
 public class ResourceController {
 
     private final ResourceService service;
 
     @GetMapping("/users")
-    public Page<User> list(@Filter Specification<User> specification, Pageable pageable) {
+    @Operation(summary = "list users. One may filter, sort, and page the result")
+    public List<User> list(
+            @Parameter(
+                    description =
+                            "Filter parameters as defined at https://github.com/turkraft/spring-filter. May be absent."
+                                    + "\nExample: /resource/users?filter= id:5 or (age > 20 and age < 50)",
+                    schema = @Schema(type = "string")
+            )
+            @Filter
+            @Nullable
+            Specification<User> specification,
+
+            @ParameterObject
+            @PageableDefault(size = 3)
+            Pageable pageable
+    ) {
         return service.getUsers(specification, pageable);
     }
 
