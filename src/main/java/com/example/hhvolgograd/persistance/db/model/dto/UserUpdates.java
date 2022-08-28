57cf82843c54d9785ed81bba7b670152cedd357e
zapisator <zapisator@logan-club.ru>
2022-08-28 17:49:12 +0300

    User updating support

--- src/main/java/com/example/hhvolgograd/persistance/db/model/dto/UserUpdates.java
+++ src/main/java/com/example/hhvolgograd/persistance/db/model/dto/UserUpdates.java
@@ -12,20 +12,25 @@ import java.util.function.Supplier;
 
 @Getter
 @NoArgsConstructor
-public class UserUpdates {
-    private final Map<String, Consumer<String>> setMethods = Map.of(
-            "name", value -> name = new DtoProperties<>("name", value),
-            "age", value -> age = new DtoProperties<>("age", (value == null ? null : Long.valueOf(value))),
-            "email", value -> email = new DtoProperties<>("email", value)
+public class UserUpdates implements EntityUpdates {
+    private static final Map<String, Class<?>> CLASS = Map.of(
+            "name", String.class,
+            "age", Integer.class,
+            "email", String.class
     );
-    private final Map<String, Supplier<DtoProperties<?>>> getMethods = Map.of(
+    private final Map<String, Consumer<String>> setMethods = Map.of(
+            "name", value -> name = new UpdateProperties<>("name", value),
+            "age", value -> age = new UpdateProperties<>("age", (value.equals("null") ? null : Integer.valueOf(value))),
+            "email", value -> email = new UpdateProperties<>("email", value)
+    );
+    private final Map<String, Supplier<UpdateProperties<?>>> getMethods = Map.of(
             "name", this::getName,
             "age", this::getAge,
             "email", this::getEmail
     );
-    private DtoProperties<String> name = new DtoProperties<>();
-    private DtoProperties<Long> age = new DtoProperties<>();
-    private DtoProperties<String> email = new DtoProperties<>();
+    private UpdateProperties<String> name = new UpdateProperties<>();
+    private UpdateProperties<Integer> age = new UpdateProperties<>();
+    private UpdateProperties<String> email = new UpdateProperties<>();
 
     public static UserUpdates create(Map<String, String> updates) {
         val user = new UserUpdates();
@@ -34,10 +39,15 @@ public class UserUpdates {
         return user;
     }
 
-    public void set(String fieldName, String value) {
-        setMethods.get(fieldName).accept(value);
+    public static Class<?> type(String fieldName) {
+        return CLASS.get(fieldName);
     }
 
+    public void set(String fieldName, String value) {
+        getSetMethods().get(fieldName).accept(value);
+    }
+
+    @Override
     public Collection<String> fieldNames() {
         return List.of(
                 "name",
@@ -45,5 +55,6 @@ public class UserUpdates {
                 "email"
         );
     }
+
 
 }
