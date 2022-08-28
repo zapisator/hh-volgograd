e3462fb0aa351235459ce9d40f52279da9b6f4bd
zapisator <zapisator@logan-club.ru>
2022-08-28 17:48:54 +0300

    Interface for entity updating

---
+++ src/main/java/com/example/hhvolgograd/persistance/db/model/dto/EntityUpdates.java
@@ -0,0 +1,13 @@
+package com.example.hhvolgograd.persistance.db.model.dto;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public interface EntityUpdates {
+
+    Map<String, Supplier<UpdateProperties<?>>> getGetMethods();
+    Map<String, Consumer<String>> getSetMethods();
+    Collection<String> fieldNames();
+}
