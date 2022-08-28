fc47c39f0b1328d6d48238e45fe565a504057a16
zapisator <zapisator@logan-club.ru>
2022-08-28 17:50:51 +0300

    Phone entity now requires value should have printable characters only

--- src/main/java/com/example/hhvolgograd/persistance/db/model/Phone.java
+++ src/main/java/com/example/hhvolgograd/persistance/db/model/Phone.java
@@ -16,6 +16,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "phones")
